f72d89724bda0ba093c53f0d13107360ff712947
Vipassana <vipassana@vipassana-AO722.(none)>
2013-07-03 17:18:51 +0530

    changed db settings

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -38,7 +38,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'tiny',  # Or path to database file if using sqlite3.
             'USER': 'vipassana',                      # Not used with sqlite3.  
-	    'PASSWORD': '#JWarnes0821',                  # Not used with sqlite3.
+	    'PASSWORD': '*****',                  # Not used with sqlite3.
             'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         }
