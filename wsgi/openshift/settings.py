d87baefe3a64cad3b10ca9a97518934af97a0153
Vipassana <vipassana@vipassana-AO722.(none)>
2013-07-05 00:50:27 +0530

    Settings changewq

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -80,7 +80,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''#os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -93,7 +93,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    '/home/vipassana/GitHub/tiny/wsgi/openshift/static',    
+#    '/home/vipassana/GitHub/tiny/wsgi/openshift/static',    
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
